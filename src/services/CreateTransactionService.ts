11e0b5fc233b207a44c92b9101c3070fd9da5e9f
alexAlvesDias <59662499+alexAlvesDias@users.noreply.github.com>
2020-08-30 22:29:18 -0300

    Projeto NodeJS

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,13 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const Transactions = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+    return Transactions;
   }
 }
 
